7682aa9456bb383df76b4b547b3131d1dfb68a1b
wayne-l <sduwyl@126.com>
2018-05-15 17:27:01 +0800

    Add files via upload

---
+++ src/main/java/examples/HelloController.java
@@ -0,0 +1,11 @@
+package examples;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HelloController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
