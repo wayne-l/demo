e70c1813f6276e7466f567de82ef563366db5d02
wayne-l <sduwyl@126.com>
2018-05-27 16:26:41 +0800

    add zhushi

--- src/main/java/examples/HelloController.java
+++ src/main/java/examples/HelloController.java
@@ -1,16 +1,47 @@
 package examples;
 
+import groovy.util.logging.Log;
+import groovy.util.logging.Log4j;
+import groovy.util.logging.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HelloController {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/index")
-    public String index(){
+    public String index(HttpServletRequest request) {
+        log.info("request client info:"+request.getRemoteAddr());
         return "index";
     }
 
     @RequestMapping("/life")
-    public String life(){
+    public String life() {
         return "life";
+    }
+
+    @RequestMapping("/shoot")
+    public String qiubite() {
+        return "4";
+    }
+
+    @RequestMapping("/support")
+    public String zoumadeng() {
+        return "15";
+    }
+
+    @RequestMapping("/hug")
+    public String hug() {
+        return "30";
+    }
+
+    @RequestMapping("/tree")
+    public String greenTree() {
+        return "41";
     }
 }
--- src/main/java/examples/Test.java
+++ src/main/java/examples/Test.java
@@ -1,86 +1,88 @@
-
-package examples;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * @author : wangyulong
- * @since: 2018/5/25 11:56
- */
-public class Test {
-
-    public static void main(String args[]) {
-        //master mofigy 1
-        //dev modify test rebase
-        FileReader reader = null;
-        //dev modify test rebase 2
-        BufferedReader br = null;
-        FileWriter writer = null;
-        BufferedWriter bw = null;
-        //amend use
-        //amend 2
-        try {
-            reader = new FileReader("/Users/wangyulong/Documents/working/orderFlow.txt");
-            br = new BufferedReader(reader);
-            StringBuilder sb = new StringBuilder("");
-            String str = null;
-            List<String> result = new ArrayList<String>();
-            while ((str = br.readLine()) != null) {
-                result.add(str);
-            }
-            for(int j = 0; j < 8; j++){
-                try {
-                    StringBuilder sbb = new StringBuilder("");
-                    writer = new FileWriter("/Users/wangyulong/Documents/working/o"+j+".txt");
-                    bw = new BufferedWriter(writer);
-                    for (int i = 0; i < 125; i++) {
-                        for (String ss: result) {
-                            if (ss.contains("order_flow_xxx")) {
-                                ss = ss.replace("xxx", "" + (j * 125 + i));
-                            }
-                            sbb.append(ss).append("\n");
-                        }
-
-                    }
-                    bw.write(sbb.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }finally {
-                    try {
-                        bw.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                br.close();
-                reader.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                bw.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-
-    }
-
-}
+//
+//package examples;
+//
+//import java.io.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * @author : wangyulong
+// * @since: 2018/5/25 11:56
+// */
+//public class Test {
+//
+//    public static void main(String args[]) {
+//        //master mofigy 1
+//        //dev modify test rebase
+//        FileReader reader = null;
+//        //dev modify test rebase 2
+//        BufferedReader br = null;
+//        FileWriter writer = null;
+//        BufferedWriter bw = null;
+//        //amend use
+//        //amend 2
+//        //test status2
+//
+//        try {
+//            reader = new FileReader("/Users/wangyulong/Documents/working/orderFlow.txt");
+//            br = new BufferedReader(reader);
+//            StringBuilder sb = new StringBuilder("");
+//            String str = null;
+//            List<String> result = new ArrayList<String>();
+//            while ((str = br.readLine()) != null) {
+//                result.add(str);
+//            }
+//            for(int j = 0; j < 8; j++){
+//                try {
+//                    StringBuilder sbb = new StringBuilder("");
+//                    writer = new FileWriter("/Users/wangyulong/Documents/working/o"+j+".txt");
+//                    bw = new BufferedWriter(writer);
+//                    for (int i = 0; i < 125; i++) {
+//                        for (String ss: result) {
+//                            if (ss.contains("order_flow_xxx")) {
+//                                ss = ss.replace("xxx", "" + (j * 125 + i));
+//                            }
+//                            sbb.append(ss).append("\n");
+//                        }
+//
+//                    }
+//                    bw.write(sbb.toString());
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }finally {
+//                    try {
+//                        bw.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                br.close();
+//                reader.close();
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            try {
+//                bw.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                writer.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//
+//
+//    }
+//
+//}
